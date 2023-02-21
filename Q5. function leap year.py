12ac2cf040e1946bbb8d58d927f7a69f2659f94c
Nilesh Thengde <nileshthengde@dietms.org>
2023-02-21 14:45:36 +0530

    Update Q5. function leap year.py

--- Q5. function leap year.py
+++ Q5. function leap year.py
@@ -13,21 +13,30 @@
 
 # Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
 
+# def is_leap(year):
+#     leap = False
+#     if(year%4 == 0):
+#         return True
+#     elif(year%100 == 0):
+#         return True
+#     elif(year%400 == 0):
+#             return True
+#     else:
+#         return False
+
+#     return leap
+
+# year = int(input())
+
+# print(is_leap(year))
+
 def is_leap(year):
-    leap = False
-    if(year%4 == 0):
+    if(year%4 == 0 )and (year%4 ==0 and year%100 != 0)or(year%100 == 0 and year%400 ==0):
         return True
-    elif(year%100 == 0):
-        return True
-    elif(year%400 == 0):
-            return True
     else:
         return False
 
-    return leap
-
 year = int(input())
-
 print(is_leap(year))
 
 
