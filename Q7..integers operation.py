b32e9c4896fb5a0dc6ad4fb9fe54a2f93bfb1379
Nilesh Thengde <nileshthengde@dietms.org>
2023-02-21 21:48:31 +0530

    Add files via upload

---
+++ Q7..integers operation.py
@@ -0,0 +1,8 @@
+#Read 4 numbers a, b, c, and d and print the results of a^b + c^d\
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print((a**b)+(c**d))
---
+++ Q8.numerical tringle.py
@@ -0,0 +1,13 @@
+# You are given a positive integer n. Print a numerical triangle of height n-1 like the one below:
+
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# ......
+# Can you do it using only arithmetic operations, a single for loop and print statement?
+
+
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(i, )
